5beeb630972e8b632212f3a4e9e7a817b67b6788
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-02-05 09:09:18 +0100

    Resolver ejercicio29

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,14 @@
 package com.campusdual.classroom;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            FileReader fr = new FileReader("noexisto.txt");
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+        }
     }
 }
